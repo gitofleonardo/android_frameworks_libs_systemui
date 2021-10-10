657297a0ee2d221467135166f762907ea7e255e8
TheScarastic <warabhishek@gmail.com>
2021-10-10 17:12:42 +0000

    iconloaderlib: Theme unthemed adaptive icons
    Change-Id: I72ed70ff5a87bfcdc69df5158e59fcdb367a9b94
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
+++ iconloaderlib/src/com/android/launcher3/icons/IconProvider.java
@@ -172,6 +172,11 @@ public class IconProvider {
         }
 
         ThemeData td = getThemedIconMap().get(packageName);
+
+        if (td == null && mThemedIconMap != DISABLED_MAP) {
+            td = new ThemeData(mContext.getResources(), icon);
+        }
+
         return td != null ? td.wrapDrawable(icon, iconType) : icon;
     }
 
